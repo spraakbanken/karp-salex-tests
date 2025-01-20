b41c4b980cc2a57c33ebdf716211bf2ec33e5353
Nick Smallbone <nick@smallbone.se>
2025-01-20 18:06:33 +0100

    Add support for endastDigitalt field

--- utils/salex.py
+++ utils/salex.py
@@ -4,29 +4,34 @@ import karp.foundation.json as json
 from copy import deepcopy
 
 
-def is_visible(path, entry):
-    # TODO: support endastDigitalt
+def entry_is_visible(entry):
+    return entry.get("visas", True)
+
+
+def entry_is_visible_in_printed_book(entry):
+    return entry_is_visible(entry) and not entry.get("endastDigitalt", False)
+
+
+def is_visible(path, entry, test=entry_is_visible):
     return True
     for i in range(len(path)):
-        visas_path = path[:i] + ["visas"]
-        if json.has_path(visas_path, entry.entry) and not json.get_path(visas_path, entry.entry):
+        if not test(json.get_path(path[:-1], entry)):
             return False
 
     return True
 
 
-def trim_invisible(data):
+def trim_invisible(data, test=entry_is_visible):
     paths = list(json.all_paths(data))  # compute up front since we will be modifying data
     for path in paths:
-        if path and path[-1] == "visas":
-            if not json.has_path(path, data):
-                continue  # already deleted
+        if not json.has_path(path, data):
+            continue  # already deleted
 
-            if not json.get_path(path, data):
-                json.del_path(path[:-1], data)
+        if not test(json.get_path(path, data)):
+            json.del_path(path, data)
 
 
-def visible_part(data):
+def visible_part(data, test=entry_is_visible):
     data = deepcopy(data)
-    trim_invisible(data)
+    trim_invisible(data, test)
     return data
