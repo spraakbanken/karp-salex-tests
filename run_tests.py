4d967dbddab39233d91f9ff97b65b96632fa17d4
Nick Smallbone <nicsma@chalmers.se>
2025-02-18 11:42:29 +0100

    add field info; remove more old stuff

--- run_tests.py
+++ run_tests.py
@@ -3,22 +3,28 @@ from tests.ordled_agreement import test_ordled_agreement
 from tests.references import test_references
 from tests.funny_characters import test_funny_characters
 from tests.mismatched_brackets_etc import test_mismatched_brackets_etc
+from tests.field_info import test_field_info
 from tqdm import tqdm
 from functools import partial
 from utils.inflection import Inflection
 from itertools import islice
 
-#entry_start = 100000
-#entry_stop = 110000
-entry_start = None
-entry_stop = None
+test_on_subset = False
+if test_on_subset:
+    entry_start = 100000
+    entry_stop = 110000
+else:
+    entry_start = None
+    entry_stop = None
 
+resource_config = resource_queries.by_resource_id("salex", expand_plugins=False).config
 entries = list(tqdm(islice(entry_queries.all_entries("salex", expand_plugins=False), entry_start, entry_stop), desc="Reading entries"))
 inflection = Inflection(entry_queries, entries)
 
 warnings = []
-warnings += test_ordled_agreement(entries)
-warnings += test_references(entries, inflection=inflection)
-warnings += test_funny_characters(entries)
-warnings += test_mismatched_brackets_etc(entries)
+warnings += test_field_info(resource_config, entries)
+#warnings += test_ordled_agreement(entries)
+#warnings += test_references(entries, inflection=inflection)
+#warnings += test_funny_characters(entries)
+#warnings += test_mismatched_brackets_etc(entries)
 write_warnings("results", warnings)
---
+++ tests/field_info.py
@@ -0,0 +1,106 @@
+from karp.foundation import json
+from collections import defaultdict, Counter
+from dataclasses import dataclass
+from utils.testing import TestWarning
+
+@dataclass
+class Statistics:
+    field: str
+    present: int
+    total: int
+
+    @property
+    def missing(self):
+        return self.total - self.present
+
+    @property
+    def missing_freq(self):
+        return self.missing / self.total
+
+    @property
+    def present_freq(self):
+        return self.present / self.total
+
+def resource_statistics(resource_config, entries):
+    present_counts = Counter()
+    total_counts = Counter()
+
+    for entry in entries:
+        count_frequency([], resource_config.entry_field_config(), entry.entry, present_counts, total_counts)
+
+    for field, total_count in total_counts.items():
+        present_count = present_counts[field]
+
+        if field != "":
+            yield Statistics(field, present_count, total_count)
+
+
+def count_frequency(path, field_config, data, present_counts, total_counts):
+    if field_config.virtual:
+        return
+
+    field_name = ".".join(path)
+
+    if field_config.collection:
+        values = data
+    else:
+        values = [data]
+
+    for value in values:
+        total_counts[field_name] += 1
+        present_counts[field_name] += 1
+
+        if field_config.type == "object":
+            for sub_name, sub_config in field_config.fields.items():
+                if sub_name in value:
+                    count_frequency(
+                        path + [sub_name],
+                        sub_config,
+                        value[sub_name],
+                        present_counts,
+                        total_counts,
+                    )
+                elif not sub_config.virtual:
+                    total_counts[".".join(path + [sub_name])] += 1
+
+            # Find extra fields not declared in the resource config
+            for sub_name in value:
+                if sub_name not in field_config.fields:
+                    total_counts[".".join(path + [sub_name])] += 1
+                    present_counts[".".join(path + [sub_name])] += 1
+
+
+@dataclass(frozen=True)
+class FieldStatistics(TestWarning):
+    _category: str
+    statistics: Statistics
+
+    def category(self):
+        return self._category
+
+    def to_dict(self):
+        frequency = f"{self.statistics.present_freq*100:.2f}% ({self.statistics.present})"
+        return {
+            "Fält": self.statistics.field,
+            "Frekvens": frequency
+        }
+
+def test_field_info(resource_config, entries):
+    statistics = list(resource_statistics(resource_config, entries))
+    always_present = [s for s in statistics if s.missing_freq == 0]
+    usually_present = [s for s in statistics if 0 < s.missing_freq <= 0.05]
+    usually_absent = [s for s in statistics if s.present_freq <= 0.05]
+
+    for s in always_present:
+        yield FieldStatistics("Always present", s)
+
+    for s in usually_present:
+        yield FieldStatistics("Usally present", s)
+
+    for s in usually_absent:
+        yield FieldStatistics("Usually absent", s)
+
+    resource_config_fields = set(resource_config.nested_fields())
+    for s in statistics:
+        if s.field not in resource_config_fields:
+            yield FieldStatistics("Extra fields", s)
